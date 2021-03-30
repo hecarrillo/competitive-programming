75480e266af9cfd9514e5722dc6a93920bb2ca98
hecarrillo <technik.und.spiel@gmail.com>
2021-03-30 15:27:34 -0600

    delete in linkedList implemented

--- linkedList.cpp
+++ linkedList.cpp
@@ -43,6 +43,24 @@ void printLLvalues(linkedList * list){
   cout << current -> value << "\n";
 }
 
+/**
+Suponiendo que si existe el valor que se intenta borrar
+**/
+void deleteNode(linkedList * list, string name){
+  Node * last = list -> head;
+  if(last -> name == name){
+    list -> head = last -> next;
+    return;
+  }
+  Node * current = last -> next;
+  while(current -> name != name){
+    last = current;
+    current = current -> next;
+  }
+  last -> next = current -> next;
+  delete current;
+}
+
 int main() {
   int n;
   linkedList * myLinkedList = new linkedList();
@@ -55,6 +73,10 @@ int main() {
     Node * temp = new Node(val, nm);
     push(myLinkedList, temp);
   }
+  cout << "Enter the name of the Node to delete" << "\n";
+  string nameToDelete;
+  cin >> nameToDelete;
+  deleteNode(myLinkedList, nameToDelete);
 
   printLLvalues(myLinkedList);
 }
